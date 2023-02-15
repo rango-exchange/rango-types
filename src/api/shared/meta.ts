8b8b3db74b1efb33046a46555a6051e6f14e6ea4
RanGojo <rangojo@rango.exchange>
2023-02-15 16:00:53 +0300

    fix types in meta

--- src/api/shared/meta.ts
+++ src/api/shared/meta.ts
@@ -149,7 +149,7 @@ export interface CosmosChainInfo extends ChainInfoBase {
     low: number
     average: number
     high: number
-  }
+  } | null
 }
 
 /**
@@ -225,8 +225,7 @@ export interface EvmBlockchainMeta extends BlockchainMetaBase {
 
 export interface CosmosBlockchainMeta extends BlockchainMetaBase {
   type: TransactionType.COSMOS
-  chainId: string
-  info: CosmosChainInfo
+  info: CosmosChainInfo | null
 }
 
 export interface TransferBlockchainMeta extends BlockchainMetaBase {
