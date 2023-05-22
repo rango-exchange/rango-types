bbdc0234fe82ee17ff15ced6ea40b547e0e5d426
RanGojo <rangojo@rango.exchange>
2023-05-22 14:29:11 +0300

    added pending swap model

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import * as mainAPI from './api/main'
 import * as basicAPI from './api/basic'
 export * from './api/shared'
 export * from './signer'
+export * from './execution'
 export { mainAPI, basicAPI }
