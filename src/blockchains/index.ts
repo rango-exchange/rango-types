843b3f7a8fc4cc4cfb5c69f57e0d26c8733aaff2
RanGojo <rangojo@rango.exchange>
2023-09-11 21:23:03 +0300

    feat: added standard types/manifests for blockchains

---
+++ src/blockchains/index.ts
@@ -0,0 +1,2 @@
+export * from './standards'
+export * from './manifests'
---
+++ src/blockchains/standards.ts
@@ -0,0 +1,29 @@
+import { TransactionType } from '../api/shared'
+import { StdCosmosManifest, StdEvmManifest } from './manifests'
+
+export type StdBlockchain = {
+  id: string
+  type: TransactionType
+  chainId: string | null
+  icon: string
+  transactionUrl: string
+}
+
+export type StdBasicBlockchainInfo = StdBlockchain & { mnifest: null }
+
+export interface StdEvmBlockchainInfo extends StdBlockchain {
+  type: TransactionType.EVM
+  chainId: string
+  manifest: StdEvmManifest
+}
+
+export interface StdCosmosBlockchainInfo extends StdBlockchain {
+  type: TransactionType.COSMOS
+  chainId: string
+  manifest: StdCosmosManifest
+}
+
+export type StdBlockchainInfo =
+  | StdBasicBlockchainInfo
+  | StdEvmBlockchainInfo
+  | StdCosmosBlockchainInfo
