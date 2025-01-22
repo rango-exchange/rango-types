5cfdf886330939fcc9bc442af9b72b0320cbe44d
RyukTheCoder <ryuk@rango.exchange>
2025-01-22 15:30:53 +0330

    feat: add psbt types to transfer transaction

--- src/api/shared/txs/transfer.ts
+++ src/api/shared/txs/transfer.ts
@@ -2,6 +2,37 @@ import { AssetWithTicker } from '../common.js'
 import { TransactionType } from '../transactions.js'
 import { BaseTransaction } from './base.js'
 
+export type UTXOWitness = {
+  value: string,
+  script: string,
+}
+export type PSBTInput = {
+  hash: string,
+  index: number,
+  witnessUtxo: UTXOWitness | null;
+  tapInternalKey: string | null
+}
+export type PSBTOutput = {
+  outputType: "address",
+  value: string,
+  address: string
+} | {
+  outputType: "script",
+  value: string,
+  script: string
+}
+/**
+ * @property {string} psbt - Hexadecimal representation of the PSBT
+ * @property {PSBTInput[]} psbtInputs - Input being spent in the transaction
+ * @property {PSBTOutput[]} psbtOutputs - Details about where the funds will be sent, including the recipient address or script, the amount to be sent.
+ * @property {string} fee - Transaction fee
+ */
+export type PSBT = {
+  psbt: string
+  psbtInputs: PSBTInput[]
+  psbtOutputs: PSBTOutput[]
+  fee: string
+}
 /**
  * TransferTransaction. This type of transaction is used for UTXO blockchains including BTC, LTC, BCH
  *
@@ -14,6 +45,7 @@ import { BaseTransaction } from './base.js'
  * @property {string} fromWalletAddress - The source wallet address that can sign this transaction
  * @property {string} recipientAddress - The destination wallet address that the fund should be sent to
  * @property {string | null} memo - The memo of transaction, can be null
+ * @property {PSBT | null} psbt - PSBT object containing the hexadecimal representation of the PSBT, along with details about its inputs, outputs, and the transaction fee.
  *
  */
 export interface Transfer extends BaseTransaction {
@@ -25,6 +57,7 @@ export interface Transfer extends BaseTransaction {
   fromWalletAddress: string
   recipientAddress: string
   memo: string | null
+  psbt: PSBT
 }
 
 export const isTransferTransaction = (transaction: {
